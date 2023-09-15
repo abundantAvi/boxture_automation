da87d663cbe955756ec1ea07537cd48e453c62f9
avirajBoxture <107924052+abundantAvi@users.noreply.github.com>
2023-09-15 09:13:10 +0530

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   viewportWidth: 1600,
   viewportHeight: 900,
+  defaultCommandTimeout: 10000,
+  watchForFileChanges: false,
+
   e2e: {
-    baseUrl: 'https://oms.staging.boxture.com',
+    baseUrl: "https://oms.staging.boxture.com",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
